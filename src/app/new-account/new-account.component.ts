c985ba53a7aabcca5db1c5a51cb290f1ed24f3cd
Yonatan <yonatan.ramirez557@gmail.com>
2018-06-22 16:07:17 -0700

    creates accountsService to keep track of data in one place

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,24 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from './../accounts.service';
+import { Component } from '@angular/core';
 import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [AccountsService, LoggingService],
+
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService) {
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
 
   } // binding to property
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
+    this.accountsService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
 
   }
